fee93c7c16392656f434eb2b5da422654637685c
RobertaCris <robertacgsilva@hotmail.com>
2024-05-15 15:46:45 -0300

    first commit

---
+++ resources/variables.py
@@ -0,0 +1,10 @@
+# Variaveis
+url = 'https://restful-booker.herokuapp.com'
+content_type = 'application/json'
+firstname = 'Laura'
+lastname = 'Moreno'
+totalprice = 150
+depositpaid = False
+bookingdates = dict(checkin='2024-04-27', checkout='2024-04-27')
+additionalneeds = 'Breakfast'
+
